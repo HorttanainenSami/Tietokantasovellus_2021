0a1139de79e90a975f67b98d1d3214ededdc9212
Horttanainen Sami <horttsam@cubbli20-gold.local>
2021-03-14 17:42:45 +0200

    FIX query to fetch all users active chats
    Fixed query to get correct data from query

--- chat.py
+++ chat.py
@@ -19,10 +19,12 @@ def chat_active(creator_id, advertisement_id):
     return chat_id
 
 def chat_get_all(participant_id):
-    sql = 'SELECT p.chat_id, c.advertisement_id, (SELECT u.username from users as u, participant as p1 WHERE u.id=p1.participant_id AND p1.chat_id=p.chat_id AND p1.participant_id!=:participant_id ) FROM participant as p , chat as c WHERE c.id = p.chat_id AND p.participant_id=:participant_id'
-    result = db.session.execute(sql, {"participant_id":participant_id})
-    return result.fetchall()
+    sql = 'SELECT x.chat_id,c.advertisement_id, u.username, u.id, x.content, x.created_at, x.unread, x.creator_id FROM chat as c, participant as p,participant as p1, users as u, (SELECT COUNT(CASE WHEN creator_id=:p_id THEN NULL ELSE CASE WHEN is_read=FALSE THEN TRUE ELSE NULL END END) OVER(PARTITION BY chat_id) as unread, ROW_NUMBER() OVER (PARTITION BY chat_id ORDER BY created_at DESC) as rownum, * FROM message) X WHERE rownum=1 AND c.id=x.chat_id AND p.chat_id=x.chat_id AND p.participant_id=:p_id AND p1.chat_id=p.chat_id AND p1.participant_id!=p.participant_id AND u.id=p1.participant_id'
+    ## valitsee kaikki viimeisimmäksi lähetetyt viestit,mikä chat, kenen lähettämä, milloin, mitä sisältää ja kuinka monta lukematonta viestiä sinulla on kyseisessä chatissa 
 
+    result = db.session.execute(sql, {"p_id":participant_id})
+    return result.fetchall()
+    
 def chat_create(creator_id, advertisement_id, content):
     # create chat db
     sql = "INSERT INTO chat (advertisement_id) VALUES (:advertisement_id) RETURNING id"
@@ -42,7 +44,7 @@ def chat_create(creator_id, advertisement_id, content):
 
     
 def chat_message_send(chat_id, message, creator_id):
-    sql = "INSERT INTO message (creator_id, chat_id, content) VALUES (:creator_id, :chat_id, :content)"
+    sql = "INSERT INTO message (creator_id, chat_id, content, created_at, is_read) VALUES (:creator_id, :chat_id, :content, NOW(), FALSE)"
     db.session.execute(sql, {"creator_id":creator_id, "chat_id":chat_id, "content":message})
     db.session.commit()
     return "OK"
