07b25cdb884a04987425b4052c4837d5ed8d351b
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-03-17 13:51:59 +0200

    Change db to contain addresses as TEXT

--- schema.sql
+++ schema.sql
@@ -1,29 +1,17 @@
-CREATE TABLE postaladdress (
-  id SERIAL PRIMARY KEY,
-  postcode TEXT,
-  post_office TEXT
-);
-
-CREATE TABLE localaddress (
-  id SERIAL PRIMARY KEY,
-  address TEXT,
-  postaladdress_id INTEGER REFERENCES postaladdress
-);
-
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username TEXT,
   password TEXT,
   created_at TIMESTAMP,
-  avatar BYTEA
-  reside INTEGER REFERENCES localaddress,
+  avatar BYTEA,
+  reside TEXT,
   info TEXT
 );
 
 CREATE TABLE advertisement (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users ON DELETE CASCADE,
-  localAddress_id INTEGER REFERENCES localaddress,
+  localAddress_id TEXT,
   published BOOLEAN,
   published_at TIMESTAMP, 
   header TEXT,
